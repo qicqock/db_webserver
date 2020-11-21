b3a51f30e32ecb8e362aae7085ef052610fa2baf
qicqock <qicqockqvgiq@gmail.com>
2020-11-22 03:25:27 +0900

    connect db in stafflist

--- routes/staff.js
+++ routes/staff.js
@@ -2,8 +2,32 @@ var express = require('express');
 var router = express.Router();
 var connection = require('./db');
 
+
+var ep_list = null;
+
 router.get('/', function(req, res, next) {
-    res.render('../views/chanwoong/staff', { title: 'Staff' , cust_info:null});
+    var sql1 = 'select * from EMPLOYEE natural join PERSON';
+    connection.query(sql1, function (error, result, fields) {
+        if (error) {
+            console.log("error발생");
+            console.log(error);
+        }
+        ep_list = result;
+        console.log(ep_list);
+    })
+    res.render('../views/chanwoong/staff', {
+        title: 'Staff' ,
+        cust_info:null,
+        staff_list : ep_list
+    });
 });
+router.post('/', function(req, res, next) {
+    res.render('../views/chanwoong/staff', {
+        title: 'Staff' ,
+        cust_info:null,
+        staff_list : ep_list
+    });
+});
+
 
 module.exports = router;
